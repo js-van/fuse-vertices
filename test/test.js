d59b7715c17f794850d43796eab75f315833a1b6
Mikola Lysenko <mikolalysenko@gmail.com>
2013-03-15 16:00:32 -0500

    adding test cases

--- test/test.js
+++ test/test.js
@@ -1,3 +1,17 @@
-var cells = [[0,1], [2,3]];
-var positions = [[0], [1], [1.000000001], [2]];
-console.log(require("../index.js")(cells, positions));
+var fuseVertices = require("../index.js")
+
+require("tap").test("fuseVertices", function(t) {
+
+  var cells = [[0,1], [2,3]];
+  var positions = [[0], [1], [1.000000001], [2]];
+  var result = require("../index.js")(cells, positions);
+  
+  t.equals(result.positions.length, 3)
+  t.equals(result.cells[0][0], 0)
+  t.equals(result.cells[0][1], 1)
+  t.equals(result.cells[1][0], 1)
+  t.equals(result.cells[1][2], 2)
+
+  t.end()
+})
+
